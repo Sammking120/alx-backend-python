cf816d5f1ad67100b651d30d0c50f67223218dda
Sammking120 <sammking120@gmail.com>
2025-06-01 20:07:20 +0300

    initiate another change on massaging_app urls

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("We made it, it works!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
